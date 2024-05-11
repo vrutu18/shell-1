064eeb9222380b7106e561d5f565dd07c711457f
vrutanki <vrutankiramteke@gmail.com>
2024-05-12 00:23:33 +0530

    print echo command in frontend with colour code

--- frontend1.sh
+++ frontend1.sh
@@ -1,16 +1,16 @@
-echo Installing nginx
+echo -e "\e[33mInstalling nginx\e[0m"
 dnf install nginx -y
-echo copying expense.conf
+echo -e "\e[33m copying expense.conf\e[0m"
 cp expense.conf/etc/nginx/default.d/expense.conf
-echo reboot content
+echo -e "\e[33mreboot content\e[0m"
 systemctl enable nginx
 systemctl start nginx
-echo remove content
+echo -e "\e[33m remove content\e[0m"
 rm -rf /usr/share/nginx/html/*
-echo download content
+echo -e "\e[33m download content\e[0m"
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip
-echo abstract content
+echo -e "\e[33m abstract content\e[0m"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
-echo restart content
+echo -e "\e[33m restart content\e[0m"
 systemctl restart nginx
