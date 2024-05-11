acfa4e04e33ded411f73b702ed7478d33dea540f
vrutanki <vrutankiramteke@gmail.com>
2024-05-11 21:59:55 +0530

    update frontend1.sh with nginx conf

--- frontend1.sh
+++ frontend1.sh
@@ -1,9 +1,10 @@
 dnf install nginx -y
+cp expense.conf/etc/nginx/default.d/expense.conf
 systemctl enable nginx
 systemctl start nginx
 rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
-cp expense.conf/etc/nginx/default.d/expense.conf
+
 systemctl restart nginx
