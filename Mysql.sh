5d362787ba18de70963808377ed863bc2450ef9e
vrutanki <vrutankiramteke@gmail.com>
2024-05-12 14:10:41 +0530

    special variable data in mysql

--- Mysql.sh
+++ Mysql.sh
@@ -1,4 +1,12 @@
+echo -e "\e[33mInstalling nginx\e[0m"
 dnf install mysql-server -y
+echo exist status - $?
+
+echo -e "\e[33mInstalling nginx\e[0m"
 systemctl enable mysqld
 systemctl start mysqld
-mysql_secure_installation --set-root-pass ExpenseApp@1
+echo exist status - $?
+
+echo -e "\e[33mInstalling nginx\e[0m"
+mysql_secure_installation --set-root-pass $1
+echo exist status - $?
